bb06aa96051f5cad1fdab7631478b962c7e12369
Kim-Ji-Min <dimdee@naver.com>
2020-04-14 00:36:11 +0900

    painting canvas w,h change and canvas events modify

--- painting/js/app.js
+++ painting/js/app.js
@@ -1,5 +1,11 @@
 //var
 const canvas = document.getElementById('jsCanvas');
+canvas.width = 600;
+canvas.height = 500;
+const ctx = canvas.getContext('2d');
+ctx.strokeStyle = '#2c2c2c';
+ctx.lineWidth = 2.5;
+
 let painting = false;
 
 //function
@@ -7,23 +13,31 @@ function stopPainting() {
     painting = false;
 }
 
+function startPainting() {
+    painting = true;
+}
+
 function onMouseMove(event) {
     const x = event.offsetX;
     const y = event.offsetY;
+    if(!painting){
+        // console.log('creating path in', x, y);
+        ctx.beginPath();
+        ctx.moveTo(x, y);
+    }else{
+        // console.log('creating line in', x, y);
+        ctx.lineTo(x, y);
+        ctx.stroke();
+    }
 }
 
 function onMouseDown(event) {
     painting = true;
 }
 
-function onMouseUp(event) {
-    stopPainting();
-}
-
-
 if (canvas){
     canvas.addEventListener('mousemove', onMouseMove);
-    canvas.addEventListener('mousedown', onMouseDown);
-    canvas.addEventListener('mouseup', onMouseUp);
+    canvas.addEventListener('mousedown', startPainting);
+    canvas.addEventListener('mouseup', stopPainting);
     canvas.addEventListener('mouseleave', stopPainting);
 }
